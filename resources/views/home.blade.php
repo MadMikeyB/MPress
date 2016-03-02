ab2119d2d09727ccca74898acf341f1745333cb8
Michael <mikeybsaccount@gmail.com>
2016-03-02 21:34:51 +0000

    Comments display on Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,23 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    You are logged in!
                     <h3>Your Posts</h3>
                     <div class="list-group">
-                    @foreach ( $posts as $post )
-                        <a href="/read/{{ $post->slug }}" class="list-group-item">{{ $post->title }}</a>
-                    @endforeach
+                    {{-- This users Posts --}}
+                    @unless ( $posts->isEmpty() )
+                        @foreach ( $posts as $post )
+                            <a href="/read/{{ $post->slug }}" class="list-group-item">
+                                {{ $post->title }}
+                                @unless ( $post->comments->isEmpty() )
+                                    @if ( $post->comments[0]->author_id === Auth::user()->id )
+                                        <span class="badge badge-red">You have commented!</span>
+                                    @endif
+                                @endunless
+                            </a>
+                        @endforeach
+                    @endunless
                     </div>
-
-                    <h3>You Commented on:</h3>
-
                 </div>
             </div>
         </div>
