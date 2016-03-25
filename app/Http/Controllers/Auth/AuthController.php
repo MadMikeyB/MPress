7a5cdad1bcb92dc878255e6d95eab86a07ec284b
Michael <mikeybsaccount@gmail.com>
2016-03-25 12:59:07 +0000

    SQLite more strict with SQL Integrity Constraints

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -66,6 +66,7 @@ class AuthController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'nickname' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
